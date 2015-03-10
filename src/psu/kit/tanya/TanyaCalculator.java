0bb694a2bc575eeb9292d548ffc0667196e4ff5c
Golovin_Vladislav <vladick95@gmail.com>
2015-03-10 20:40:42 +0500

    Load my solution

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -1,8 +1,40 @@
-package psu.kit.tanya;
+﻿package psu.kit.tanya;
 
 public class TanyaCalculator {
 
-    public TanyaReaction calculate(String s, String t) {
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+     public TanyaReaction calculate(String s, String t) {
+        String tCopy =t;
+        String sCopy=s;
+        int lenS=sCopy.length();
+        int yay=0;
+        int whoops=0;
+        while(lenS>0) {
+            if (tCopy.indexOf(sCopy.charAt(0))!=-1) {
+                ++yay;
+                tCopy = tCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
+                sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
+                --lenS;
+            } else{
+
+                String sCharacter=String.valueOf(sCopy.charAt(0));
+                int indexForYay=Character.isLowerCase(sCopy.charAt(0)) ? sCopy.indexOf(sCharacter.toUpperCase()):sCopy.indexOf(sCharacter.toLowerCase());
+                if((indexForYay!=-1)&&(tCopy.indexOf(sCopy.charAt(indexForYay))!=-1)){
+                    ++yay;
+                    tCopy=tCopy.replaceFirst(String.valueOf(sCopy.charAt(indexForYay)),"");
+                    sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(indexForYay)),"");
+                    --lenS;
+                } else {
+                    sCharacter=Character.isUpperCase(sCopy.charAt(0)) ? sCharacter.toLowerCase():sCharacter.toUpperCase();
+                    if(tCopy.indexOf(sCharacter)!=-1){
+                        ++whoops;
+                        tCopy=tCopy.replaceFirst(String.valueOf(sCharacter),"");
+                    }
+                    sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
+                    --lenS;
+                }
+            }
+        }
+        reactor=new TanyaReaction(yay,whoops);
+        return reactor;
     }
 }

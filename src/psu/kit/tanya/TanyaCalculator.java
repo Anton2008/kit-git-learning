bcd5c48701c24edb0267aff63a463b7a70fb73a1
Golovin_Vladislav <vladick95@gmail.com>
2015-03-20 16:25:12 +0500

    Переделал решение, теперь проходит меньше чем за 2 секунды

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -1,11 +1,52 @@
 package psu.kit.tanya;
 
+import java.util.Hashtable;
+import java.util.Map;
+
 public class TanyaCalculator {
 
 
     public TanyaReaction calculate(String s, String t) {
 
-        throw new UnsupportedOperationException("Метод ещё не реалиозован");
+        int yay=0;
+        int whoops=0;
+        int i=0;
+        char[] sCopy=s.toCharArray();
+        char[] tCopy=t.toCharArray();
 
+        int lenT=tCopy.length;
+        int lenS=sCopy.length;
+
+        Map<Character,Integer> map=new Hashtable<Character,Integer>();
+
+        while (i<lenT){
+            map.put(tCopy[i],0);
+            ++i;
+        }
+        i=0;
+        while (i<lenT){
+            map.put(tCopy[i],map.get(tCopy[i])+1);
+            ++i;
+        }
+        i=0;
+        while (i<lenS){
+            if(map.get(sCopy[i])!=null && map.get(sCopy[i])>0){
+                ++yay;
+                map.put(sCopy[i],map.get(sCopy[i])-1);
+                sCopy[i]='_';
+            }
+            ++i;
+        }
+        i=0;
+        while (i<lenS){
+            Character a=Character.isUpperCase(sCopy[i]) ? Character.toLowerCase(sCopy[i]):Character.toUpperCase(sCopy[i]);
+            if(map.get(a)!=null && map.get(a)>0){
+                ++whoops;
+                map.put(a,map.get(a)-1);
+            }
+            ++i;
+        }
+
+        return new TanyaReaction(yay,whoops);
     }
 }

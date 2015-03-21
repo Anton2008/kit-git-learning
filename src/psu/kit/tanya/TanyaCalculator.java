68cf30ca47658dd837c33817fdd6a00314cad530
Golovin_Vladislav <vladick95@gmail.com>
2015-03-21 10:24:23 +0500

    Небольшой рефакторинг

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -7,46 +7,46 @@ public class TanyaCalculator {
 
     public TanyaReaction calculate(String s, String t) {
 
-        int yay=0;
-        int whoops=0;
-        int i=0;
-        char[] sCopy=s.toCharArray();
-        char[] tCopy=t.toCharArray();
+        int yay = 0;
+        int whoops = 0;
+        int i = 0;
+        char[] sCopy = s.toCharArray();
+        char[] tCopy = t.toCharArray();
 
-        int lenT=tCopy.length;
-        int lenS=sCopy.length;
+        int lenT = tCopy.length;
+        int lenS = sCopy.length;
 
-        Map<Character,Integer> map=new Hashtable<Character,Integer>();
+        Map<Character, Integer> map = new Hashtable<Character, Integer>();
 
-        while (i<lenT){
-            map.put(tCopy[i],0);
+        while (i < lenT) {
+            map.put(tCopy[i], 0);
             ++i;
         }
-        i=0;
-        while (i<lenT){
-            map.put(tCopy[i],map.get(tCopy[i])+1);
+        i = 0;
+        while (i < lenT) {
+            map.put(tCopy[i], map.get(tCopy[i]) + 1);
             ++i;
         }
-        i=0;
-        while (i<lenS){
-            if(map.get(sCopy[i])!=null && map.get(sCopy[i])>0){
+        i = 0;
+        while (i < lenS) {
+            if (map.get(sCopy[i]) != null && map.get(sCopy[i]) > 0) {
                 ++yay;
-                map.put(sCopy[i],map.get(sCopy[i])-1);
-                sCopy[i]='_';
+                map.put(sCopy[i], map.get(sCopy[i]) - 1);
+                sCopy[i] = '_';
             }
             ++i;
         }
-        i=0;
-        while (i<lenS){
-            Character a=Character.isUpperCase(sCopy[i]) ? Character.toLowerCase(sCopy[i]):Character.toUpperCase(sCopy[i]);
-            if(map.get(a)!=null && map.get(a)>0){
+        i = 0;
+        while (i < lenS) {
+            Character a = Character.isUpperCase(sCopy[i]) ? Character.toLowerCase(sCopy[i]) : Character.toUpperCase(sCopy[i]);
+            if (map.get(a) != null && map.get(a) > 0) {
                 ++whoops;
-                map.put(a,map.get(a)-1);
+                map.put(a, map.get(a) - 1);
             }
             ++i;
         }
 
-        return new TanyaReaction(yay,whoops);
+        return new TanyaReaction(yay, whoops);
 
     }
 }

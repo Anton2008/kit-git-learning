d43f51b583142cf6a10f48c0ac918e5b610a1158
Golovin_Vladislav <vladick95@gmail.com>
2015-03-21 09:31:25 +0500

    Исправил, теперь компилится

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -1,11 +1,9 @@
-﻿package psu.kit.tanya;
+package psu.kit.tanya;
 
 import java.util.Hashtable;
 import java.util.Map;
 
 public class TanyaCalculator {
-
-<<<<<<< HEAD
 
     public TanyaReaction calculate(String s, String t) {
 
@@ -49,41 +47,6 @@ public class TanyaCalculator {
         }
 
         return new TanyaReaction(yay,whoops);
-=======
-     public TanyaReaction calculate(String s, String t) {
-        String tCopy =t;
-        String sCopy=s;
-        int lenS=sCopy.length();
-        int yay=0;
-        int whoops=0;
-        while(lenS>0) {
-            if (tCopy.indexOf(sCopy.charAt(0))!=-1) {
-                ++yay;
-                tCopy = tCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
-                sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
-                --lenS;
-            } else{
 
-                String sCharacter=String.valueOf(sCopy.charAt(0));
-                int indexForYay=Character.isLowerCase(sCopy.charAt(0)) ? sCopy.indexOf(sCharacter.toUpperCase()):sCopy.indexOf(sCharacter.toLowerCase());
-                if((indexForYay!=-1)&&(tCopy.indexOf(sCopy.charAt(indexForYay))!=-1)){
-                    ++yay;
-                    tCopy=tCopy.replaceFirst(String.valueOf(sCopy.charAt(indexForYay)),"");
-                    sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(indexForYay)),"");
-                    --lenS;
-                } else {
-                    sCharacter=Character.isUpperCase(sCopy.charAt(0)) ? sCharacter.toLowerCase():sCharacter.toUpperCase();
-                    if(tCopy.indexOf(sCharacter)!=-1){
-                        ++whoops;
-                        tCopy=tCopy.replaceFirst(String.valueOf(sCharacter),"");
-                    }
-                    sCopy=sCopy.replaceFirst(String.valueOf(sCopy.charAt(0)),"");
-                    --lenS;
-                }
-            }
-        }
-        TanyaReaction reactor=new TanyaReaction(yay,whoops);
-        return reactor;
->>>>>>> 2abd8c1b96922b42f4823ed4ac903b11e5f3308b
     }
 }

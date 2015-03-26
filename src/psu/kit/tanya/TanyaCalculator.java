77080df089dd4f55aaeda881363ace3e969ba8cd
kioko66 <kioko.tomodachi@mail.ru>
2015-03-26 10:43:19 -1000

    @_@

--- src/psu/kit/tanya/TanyaCalculator.java
+++ src/psu/kit/tanya/TanyaCalculator.java
@@ -2,25 +2,28 @@ package psu.kit.tanya;
 
 public class TanyaCalculator {
 
+    private int convertLetterToIndex(char c) {
+        if (Character.isUpperCase(c)) {
+            return c - 'A';
+        } else {
+            return c - 'a' + 26;
+        }
+    }
+
     public TanyaReaction calculate(String s, String t) {
 
         int whoopsCount = 0;
         int yayCount = 0;
 
-        int[] countS = new int[52];
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (Character.isUpperCase(c)) {
-                countS[c - 'A']++;
-            } else countS[c - 'a' + 26]++;
-        }
-
         int[] countT = new int[52];
+        int[] countS = new int[52];
+
+        for (int i = 0; i < s.length(); i++) {
+            countS[convertLetterToIndex(s.charAt(i))]++;
+        }
+
         for (int i = 0; i < t.length(); i++) {
-            char c = t.charAt(i);
-            if (Character.isUpperCase(c)) {
-                countT[c - 'A']++;
-            } else countT[c - 'a' + 26]++;
+            countT[convertLetterToIndex(t.charAt(i))]++;
         }
 
         for (int i = 0; i < 26; i++) {
@@ -29,26 +32,16 @@ public class TanyaCalculator {
                 countS[i]--;
                 countT[i]--;
             }
-        }
-
-        for (int i = 26; i < 52; i++) {
-
-            while (countS[i] > 0 && countT[i] > 0) {
+            while (countS[i + 26] > 0 && countT[i + 26] > 0) {
                 yayCount++;
-                countS[i]--;
-                countT[i]--;
+                countS[i + 26]--;
+                countT[i + 26]--;
             }
-        }
-
-        for (int i = 0; i < 26; i++) {
             while (countS[i] > 0 && countT[i + 26] > 0) {
                 whoopsCount++;
                 countS[i]--;
                 countT[i + 26]--;
             }
-        }
-
-        for (int i = 0; i < 26; i++) {
             while (countS[i + 26] > 0 && countT[i] > 0) {
                 whoopsCount++;
                 countS[i + 26]--;
